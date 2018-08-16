d19eed6f55106f5d5f4e7aa0f3c28382c2c2e07e
Huseyn <>
2018-08-16 14:54:13 +0400

    upload

---
+++ EntityFrameworkCrudApp/EFCrudApp/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace EFCrudApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_first = new System.Windows.Forms.TextBox();
+            this.txt_last = new System.Windows.Forms.TextBox();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_cansel = new System.Windows.Forms.Button();
+            this.dgvCustomer = new System.Windows.Forms.DataGridView();
+            this.txt_adress = new System.Windows.Forms.TextBox();
+            this.CustomerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.City = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(44, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(44, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(44, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(44, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adress";
+            // 
+            // txt_first
+            // 
+            this.txt_first.Location = new System.Drawing.Point(125, 40);
+            this.txt_first.Name = "txt_first";
+            this.txt_first.Size = new System.Drawing.Size(178, 20);
+            this.txt_first.TabIndex = 4;
+            // 
+            // txt_last
+            // 
+            this.txt_last.Location = new System.Drawing.Point(125, 79);
+            this.txt_last.Name = "txt_last";
+            this.txt_last.Size = new System.Drawing.Size(178, 20);
+            this.txt_last.TabIndex = 5;
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(125, 117);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(178, 20);
+            this.txt_city.TabIndex = 6;
+            this.txt_city.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btn_save.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_save.Location = new System.Drawing.Point(42, 235);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 37);
+            this.btn_save.TabIndex = 8;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btn_delete.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_delete.Location = new System.Drawing.Point(135, 235);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(73, 37);
+            this.btn_delete.TabIndex = 9;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_cansel
+            // 
+            this.btn_cansel.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btn_cansel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_cansel.Location = new System.Drawing.Point(228, 235);
+            this.btn_cansel.Name = "btn_cansel";
+            this.btn_cansel.Size = new System.Drawing.Size(75, 37);
+            this.btn_cansel.TabIndex = 10;
+            this.btn_cansel.Text = "Cansel";
+            this.btn_cansel.UseVisualStyleBackColor = false;
+            this.btn_cansel.Click += new System.EventHandler(this.btn_cansel_Click);
+            // 
+            // dgvCustomer
+            // 
+            this.dgvCustomer.AllowUserToDeleteRows = false;
+            this.dgvCustomer.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CustomerID,
+            this.FirstName,
+            this.LastName,
+            this.City});
+            this.dgvCustomer.Location = new System.Drawing.Point(330, 12);
+            this.dgvCustomer.Name = "dgvCustomer";
+            this.dgvCustomer.ReadOnly = true;
+            this.dgvCustomer.Size = new System.Drawing.Size(424, 308);
+            this.dgvCustomer.TabIndex = 11;
+            this.dgvCustomer.DoubleClick += new System.EventHandler(this.dgvCustomer_DoubleClick);
+            // 
+            // txt_adress
+            // 
+            this.txt_adress.Location = new System.Drawing.Point(125, 154);
+            this.txt_adress.Multiline = true;
+            this.txt_adress.Name = "txt_adress";
+            this.txt_adress.Size = new System.Drawing.Size(178, 61);
+            this.txt_adress.TabIndex = 7;
+            // 
+            // CustomerID
+            // 
+            this.CustomerID.DataPropertyName = "CustomerID";
+            this.CustomerID.HeaderText = "CustomerID";
+            this.CustomerID.Name = "CustomerID";
+            this.CustomerID.ReadOnly = true;
+            this.CustomerID.Visible = false;
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FirstName.DataPropertyName = "FirstName";
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "LastName";
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // City
+            // 
+            this.City.DataPropertyName = "City";
+            this.City.HeaderText = "City";
+            this.City.Name = "City";
+            this.City.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(766, 332);
+            this.Controls.Add(this.dgvCustomer);
+            this.Controls.Add(this.btn_cansel);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.txt_adress);
+            this.Controls.Add(this.txt_city);
+            this.Controls.Add(this.txt_last);
+            this.Controls.Add(this.txt_first);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Entity Framework Crud App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_first;
+        private System.Windows.Forms.TextBox txt_last;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_cansel;
+        private System.Windows.Forms.DataGridView dgvCustomer;
+        private System.Windows.Forms.TextBox txt_adress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustomerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn City;
+    }
+}
+
---
+++ EntityFrameworkCrudApp/EFCrudApp/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFCrudApp
+{
+    public partial class Form1 : Form
+    {
+        Customer model = new Customer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cansel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            txt_first.Text = txt_last.Text = txt_city.Text = txt_adress.Text = "";
+            btn_save.Text = "Save";
+            btn_delete.Enabled = false;
+            model.CustomerID = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            model.FirstName = txt_first.Text.Trim();
+            model.LastName = txt_last.Text.Trim();
+            model.City = txt_city.Text.Trim();
+            model.Adress = txt_adress.Text.Trim();
+            using (DBEntities db = new DBEntities())
+            {
+                if (model.CustomerID == 0)
+                    db.Customers.Add(model);
+                else
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Submitted Successfully");
+        }
+
+        void PopulateDataGridView()
+        {
+            dgvCustomer.AutoGenerateColumns = false;
+            using(DBEntities db = new DBEntities())
+            {
+                dgvCustomer.DataSource = db.Customers.ToList<Customer>();
+            }
+        }
+
+        private void dgvCustomer_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvCustomer.CurrentRow.Index != -1)
+            {
+                model.CustomerID = Convert.ToInt32(dgvCustomer.CurrentRow.Cells["CustomerID"].Value);
+                using(DBEntities db = new DBEntities())
+                {
+                    model = db.Customers.Where(x => x.CustomerID == model.CustomerID).FirstOrDefault();
+                    txt_first.Text = model.FirstName;
+                    txt_last.Text = model.LastName;
+                    txt_city.Text = model.City;
+                    txt_adress.Text = model.Adress;
+                }
+                btn_save.Text = "Update";
+                btn_delete.Enabled = true;
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure to delate this record?" , "EF CRUD operation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using(DBEntities db = new DBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.Customers.Attach(model);
+                    db.Customers.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+        }
+    }
+}
